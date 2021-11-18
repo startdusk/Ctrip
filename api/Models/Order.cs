e6159dada367412179dbcd585f1da07392037b56
startdusk <522383345@qq.com>
2021-11-18 22:31:36 +0800

    add(api): add order stateless machine

--- api/Models/Order.cs
+++ api/Models/Order.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
+using Stateless;
+
 namespace Ctrip.API.Models
 {
+    // 状态
     public enum OrderStateEnum
     {
         Pending, // 订单已生成
@@ -13,13 +16,27 @@ namespace Ctrip.API.Models
         Cancelled, // 订单取消
         Refound, // 已退款
     }
+
+    // 触发事件
+    public enum OrderStateTriggerEnum
+    {
+        PlaceOrder, // 支付
+        Approve, // 支付成功
+        Reject, // 支付失败
+        Cancel, // 取消
+        Return, // 退货
+    }
+
     public class Order
     {
+        public Order()
+        {
+            StateMachineInit();
+        }
         [Key]
         public Guid Id { get; set; }
         public string UserId { get; set; }
 
-        // 一个购物车有且仅有一个用户外键
         // User是数据库外键对象，所以它不会参与数据库结构的创建
         public ApplicationUser User { get; set; }
         public ICollection<LineItem> OrderItems { get; set; }
@@ -28,5 +45,28 @@ namespace Ctrip.API.Models
         public DateTime CreateDateUTC { get; set; }
 
         public string TransactionMetadata { get; set; }
+
+        StateMachine<OrderStateEnum, OrderStateTriggerEnum> _machine;
+
+        private void StateMachineInit()
+        {
+            // 配置状态机初始化状态
+            _machine = new StateMachine<OrderStateEnum, OrderStateTriggerEnum>(OrderStateEnum.Pending);
+
+            // 配置触发动作以及状态转换
+            _machine.Configure(OrderStateEnum.Pending)
+                .Permit(OrderStateTriggerEnum.PlaceOrder, OrderStateEnum.Processing)
+                .Permit(OrderStateTriggerEnum.Cancel, OrderStateEnum.Cancelled);
+
+            _machine.Configure(OrderStateEnum.Processing)
+                .Permit(OrderStateTriggerEnum.Approve, OrderStateEnum.Completed)
+                .Permit(OrderStateTriggerEnum.Reject, OrderStateEnum.Declined);
+
+            _machine.Configure(OrderStateEnum.Declined)
+                .Permit(OrderStateTriggerEnum.PlaceOrder, OrderStateEnum.Processing);
+
+            _machine.Configure(OrderStateEnum.Completed)
+                .Permit(OrderStateTriggerEnum.Return, OrderStateEnum.Refound);
+        }
     }
 }
