b61a2592672cc2d1a700bb739b00da8be7309bab
startdusk <522383345@qq.com>
2021-11-27 21:22:13 +0800

    add(web): add home header

--- web/src/custom.d.ts
+++ web/src/custom.d.ts
@@ -1,4 +1,4 @@
-declare module '*.module.css' {
-    const css: { [key: string]: string };
-    export default css;
+declare module "*.css" {
+  const css: { [key: string]: string };
+  export default css;
 }
