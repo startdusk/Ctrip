84cc20fbd105a85e1eadd4c9152cf67fe7f844ff
startdusk <522383345@qq.com>
2021-12-03 23:40:45 +0800

    add(web): add ajax test get data

--- web/src/pages/home/HomePage.tsx
+++ web/src/pages/home/HomePage.tsx
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
-import { Row, Col, Typography } from "antd";
+import { Row, Col, Typography, Spin } from "antd";
 import {
   Header,
   Footer,
@@ -17,11 +17,53 @@ import sideImage2 from "../../assets/images/sider_2019_02-04.png";
 import sideImage3 from "../../assets/images/sider_2019_02-04-2.png";
 
 import styles from "./HomePage.module.css";
+import axios from "axios";
+import camelcaseKeys from "camelcase-keys";
 
 interface HomePageProps {}
 
 export const HomePage: React.FC<HomePageProps> = () => {
   const { t } = useTranslation();
+  const [loading, setLoading] = useState(true);
+  const [productList, setProductList] = useState<any[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  useEffect(() => {
+    const getProductList = async () => {
+      const { data } = await axios.get(
+        "http://localhost:5000/api/productCollections"
+      );
+      console.log(camelcaseKeys(data, { deep: true }));
+      setProductList(camelcaseKeys(data, { deep: true }));
+      setLoading(false);
+      setError(null);
+    };
+    try {
+      getProductList();
+    } catch (err: any) {
+      setLoading(false);
+      setError(err.message);
+    }
+  }, []);
+
+  if (loading) {
+    return (
+      <Spin
+        size={"large"}
+        style={{
+          marginTop: 200,
+          marginBottom: 200,
+          marginLeft: "auto",
+          marginRight: "auto",
+          width: "100%",
+        }}
+      />
+    );
+  }
+
+  if (error) {
+    return <div>网站出错了: {error}</div>;
+  }
+
   return (
     <>
       <Header />
@@ -42,7 +84,7 @@ export const HomePage: React.FC<HomePageProps> = () => {
             </Typography.Title>
           }
           sideImage={sideImage1}
-          products={productList1}
+          products={productList[0].touristRoutes}
         />
         <ProductCollection
           title={
@@ -51,7 +93,7 @@ export const HomePage: React.FC<HomePageProps> = () => {
             </Typography.Title>
           }
           sideImage={sideImage2}
-          products={productList2}
+          products={productList[1].touristRoutes}
         />
         <ProductCollection
           title={
@@ -60,7 +102,7 @@ export const HomePage: React.FC<HomePageProps> = () => {
             </Typography.Title>
           }
           sideImage={sideImage3}
-          products={productList3}
+          products={productList[2].touristRoutes}
         />
         <BusinessPartners />
       </div>
